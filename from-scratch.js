d80cd12551924d8b323a8a6008f89527a6015e50
gjromero6906 <gjr6906@gmail.com>
2025-11-03 09:52:24 -0500

    finished problem 3

--- from-scratch.js
+++ from-scratch.js
@@ -21,9 +21,13 @@ const getStringLengths = (strings) => {
 
 // Problem 3
 const sortWordsZtoA = (words) => {
-
+  words.sort((a, b) => {
+    if (a > b) return -1;
+    if (a < b) return 1;
+    return 0;
+  });
+  
 };
-
 /* -------------------------------------------- */
 /* --------------NUMBER PROBLEMS--------------- */
 /* -------------------------------------------- */
